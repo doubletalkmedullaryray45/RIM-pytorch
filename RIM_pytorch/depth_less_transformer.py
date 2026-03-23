8507bf01598e2cede8674902f211a6717840d8c4
lucidrains <lucidrains@gmail.com>
2026-03-23 07:50:24 -0700

    start with attention

--- RIM_pytorch/depth_less_transformer.py
+++ RIM_pytorch/depth_less_transformer.py
@@ -1,8 +1,16 @@
 from __future__ import annotations
+from functools import partial
 
 import torch
-from torch.nn import Module, ModuleList
 import torch.nn.functional as F
+from torch.nn import Module, ModuleList, Linear
+
+from einops import einsum
+from einops.layers.torch import Rearrange
+
+# constants
+
+LinearNoBias = partial(Linear, bias = False)
 
 # helper functions
 
@@ -12,6 +20,60 @@ def exists(v):
 def default(v, d):
     return v if exists(v) else d
 
+# attention
+
+class Attention(Module):
+    def __init__(
+        self,
+        dim,
+        dim_head = 64,
+        heads = 8
+    ):
+        super().__init__()
+        self.scale = dim_head ** -0.5
+        dim_inner = dim_head * heads
+
+        self.to_qkv = LinearNoBias(dim, dim_inner * 3)
+
+        self.to_out = LinearNoBias(dim_inner, dim)
+
+        self.split_heads = Rearrange('b n (h d) -> b h n d', h = heads)
+
+        self.merge_heads = Rearrange('b h n d -> b n (h d)')
+
+    def forward(
+        self,
+        tokens
+    ):
+        q, k, v = self.to_qkv(tokens).chunk(3, dim = -1)
+        q, k, v = map(self.split_heads, (q, k, v))
+
+        sim = einsum(q, k, 'b h i d, b h j d -> b h i j')
+
+        attn = sim.softmax(dim = -1)
+
+        out = einsum(attn, v, 'b h i j, b h j d -> b h i d')
+
+        out = self.merge_heads(out)
+        return self.to_out(out)
+
+# block
+
+class TransformerBlock(Module):
+    def __init__(
+        self,
+        dim,
+        dim_head = 64,
+        heads = 8
+    ):
+        super().__init__()
+
+    def forward(
+        self,
+        tokens
+    ):
+        return tokens
+
 # classes
 
 class DepthlessTransformer(Module):
@@ -20,3 +82,9 @@ class DepthlessTransformer(Module):
     ):
         super().__init__()
         raise NotImplementedError
+
+    def forward(
+        self,
+        tokens
+    ):
+        return tokens
